71e9e2fa797dc6eaf0b827c98e548849631baac0
ShimmyMySherbet <44135337+ShimmyMySherbet@users.noreply.github.com>
2022-05-12 21:17:39 +1000

    Added /trash

---
+++ SherbetVaults/Commands/TrashCommand.cs
@@ -0,0 +1,19 @@
+﻿using Cysharp.Threading.Tasks;
+using Rocket.API;
+using RocketExtensions.Models;
+using RocketExtensions.Plugins;
+using SherbetVaults.Models;
+
+namespace SherbetVaults.Commands
+{
+    [CommandName("Trash")]
+    [CommandInfo("Opens a trash can")]
+    [AllowedCaller(AllowedCaller.Player)]
+    public class TrashCommand : RocketCommand
+    {
+        public override async UniTask Execute(CommandContext context)
+        {
+            await context.LDMPlayer.OpenTrashAsync();
+        }
+    }
+}
---
+++ SherbetVaults/Models/TrashUtility.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using RocketExtensions.Models;
+using RocketExtensions.Utilities.ShimmyMySherbet.Extensions;
+using SDG.Unturned;
+
+namespace SherbetVaults.Models
+{
+    public static class TrashUtility
+    {
+        public static async Task OpenTrashAsync(this LDMPlayer player)
+        {
+            var items = new Items(7);
+            items.resize(15, 15);
+
+            await ThreadTool.RunOnGameThreadAsync(() =>
+            {
+                player.Player.inventory.updateItems(7, items);
+                player.Player.inventory.sendStorage();
+            })
+        }
+    }
+}
